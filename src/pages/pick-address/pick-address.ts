e0496604f8ae7cfb56494a73704e7fe3317aa1c8
Éderson Lehugeur <eder.lehugeur@gmail.com>
2020-01-29 13:59:45 -0300

    Armazenando os dados do pedido

--- src/pages/pick-address/pick-address.ts
+++ src/pages/pick-address/pick-address.ts
@@ -1,8 +1,10 @@
+import { CartService } from './../../services/domain/cart.service';
 import { StorageService } from './../../services/storage.service';
 import { EnderecoDTO } from './../../models/endereco.dto';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { ClienteService } from '../../services/domain/cliente.service';
+import { PedidoDTO } from '../../models/pedido.dto';
 
 @IonicPage()
 @Component({
@@ -12,12 +14,14 @@ import { ClienteService } from '../../services/domain/cliente.service';
 export class PickAddressPage {
 
   enderecos: EnderecoDTO[];
+  pedido: PedidoDTO;
 
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
     public storage: StorageService,
-    public clienteService: ClienteService) {
+    public clienteService: ClienteService,
+    public cartService: CartService) {
   }
 
   ionViewDidLoad() {
@@ -27,6 +31,22 @@ export class PickAddressPage {
       this.clienteService.findByEmail(localUser.email)
         .subscribe(response => {
           this.enderecos = response['enderecos'];
+
+          const cart = this.cartService.getCart();
+
+          this.pedido = {
+            cliente: {
+              id: response['id']
+            },
+            enderecoDeEntrega: null,
+            pagamento: null,
+            itens: cart.itens.map(item => {
+              return {
+                quantidade: item.quantidade,
+                produto: { id: item.produto.id }
+              }
+            })
+          }
         },
           error => {
             if (error.status == 403) {
@@ -36,6 +56,11 @@ export class PickAddressPage {
     } else {
       this.navCtrl.setRoot('HomePage');
     }
+  }
+
+  nextPage(endereco: EnderecoDTO) {
+    this.pedido.enderecoDeEntrega = { id: endereco.id };
+    console.log(this.pedido);
   }
 
 }
