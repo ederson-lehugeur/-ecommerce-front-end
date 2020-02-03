f8346b2b19b086000b0caaeeb1e7ad7c7be3f8f5
Éderson Lehugeur <eder.lehugeur@gmail.com>
2020-02-03 14:27:39 -0300

    Correção em baseUrl

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,5 +1,5 @@
 export const API_CONFIG = {
     //baseUrl: "http://localhost:8080",
-    baseUrl: "https://app-ecommerce-spring-ionic.herokuapp.com/",
+    baseUrl: "https://app-ecommerce-spring-ionic.herokuapp.com",
     bucketBaseUrl: "https://app-ecommerce-spring-ionic.s3-sa-east-1.amazonaws.com"
 }
